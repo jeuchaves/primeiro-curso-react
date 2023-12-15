b079e2d1e17ee89095bedf5da739f442c22fd6a3
Jeú Chaves <jeuchaveslima@gmail.com>
2023-12-15 11:30:45 -0400

    feature: Eventos no React

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Esse repositório visa eu programar juntamente com os vídeos do curso um gerenc
 
 # O que aconteceu nessa aula
 
-## Avançado em `Props`
+## `Eventos`
 
-- Podemos **definir tipos para as props**, realizando uma espécie de validação;
-- Definimos em um objeto chamado propTypes no próprio componente;
-- E ainda há a possibilidade de **definir um valor padrão**;
-- Neste caso utilizamos o objeto **defaultProps**;
+- Os **eventos de React** são os mesmos eventos do DOM;
+- Ou seja, temos eventos para responder a um click;
+- **O evento é atrelado a uma tag** que irá executá-lo;
+- Geralmente um **método** é atribuído ao evento;
+- Este método deve ser criado no componente;
--- src/App.js
+++ src/App.js
@@ -1,28 +1,14 @@
 import './App.css';
-import Frase from './components/Frase';
-import SayMyName from './components/SayMyName'
-import Pessoa from './components/Pessoa'
-import List from './components/List'
+import Evento from './components/Evento'
+import Form from './components/Form';
 
 function App() {
-  const nome = "João";
-
   return (
     <div className="App">
-      <Frase />
-      <Frase />
-      <SayMyName nome="Matheus" />
-      <SayMyName nome={nome} />
-      <SayMyName nome="Maria" />
-      <Pessoa 
-        foto="https://via.placeholder.com/150"
-        nome="Jeú Chaves"
-        idade="23"
-        peso="95.5"
-        altura="1.75"
-        genero="Masculino"
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero={1}/>
+      <Evento numero={2}/>
+      <Form />      
     </div>
   );
 }
