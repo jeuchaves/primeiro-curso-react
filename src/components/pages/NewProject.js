85d58080d02b733308245ad1ffbe9ef619fb7321
Jeú Chaves <jeuchaveslima@gmail.com>
2023-12-15 22:43:27 -0400

    feature: Salvamento de projetos em db.json

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -2,12 +2,41 @@ import styles from './NewProject.module.css';
 
 import ProjectForm from '../project/ProjectForm';
 
+import { useNavigate } from 'react-router';
+
 function NewProject() {
+
+    const Navigate = useNavigate();
+
+    function createPost(project) {
+
+        // initialize const and services
+        project.cost = 0;
+        project.services = [];
+
+        fetch("http://localhost:5000/projects", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            console.log(data);
+            // redirect
+            Navigate('/projects', {
+                message: "Projeto criado com sucesso!"
+            });
+        })
+        .catch((err) => console.log(err));
+    }
+
     return (
         <div className={styles.newProjectContainer}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm btnText="Criar Projeto"/>
+            <ProjectForm handleSubmit={createPost} btnText="Criar Projeto"/>
         </div>
     );
 }
